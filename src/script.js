afa013b9d605db8217e23d627e865db447eeb7d5
박시은 <charm516@naver.com>
2024-11-01 02:34:34 +0900

    [feat] - add 주말 박스 오피스 API

--- src/script.js
+++ src/script.js
@@ -1,8 +1,19 @@
 import config from "../config/apikey.js";
 const TARGETDT = "20241101";
 
+// 주말 박스 오피스 API
 fetch(
   `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${config.API_KEY}&targetDt=${TARGETDT}`
+)
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((error) => {
+    console.error("error", error);
+  });
+
+// 일별 박스 오피스 API
+fetch(
+  `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=${config.API_KEY}&targetDt=${TARGETDT}`
 )
   .then((res) => res.json())
   .then((data) => console.log(data))
